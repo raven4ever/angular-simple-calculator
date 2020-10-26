4d4be683c0a1cf87182f50d024368d31b629bf60
raven4ever <wrtv23@gmail.com>
2020-10-26 14:28:44 +0200

    app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputComponentComponent } from './input-component/input-component.component';
+import { ResultsComponentComponent } from './results-component/results-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponentComponent,
+    ResultsComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/results-component/results-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-component',
+  templateUrl: './results-component.component.html',
+  styleUrls: ['./results-component.component.css']
+})
+export class ResultsComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
